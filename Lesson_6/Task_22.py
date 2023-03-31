ac995b8869e7af77370edde0d3185394f288a3e9
Kopyl Tatyana <yurtochka@yandex.ru>
2023-04-01 01:56:43 +0300

    Lesson 6. Repeating lists.

--- Lesson_6/Task_22.py
+++ Lesson_6/Task_22.py
@@ -8,6 +8,5 @@ random_list = [randint(1, 20) for i in range(size_list)]
 print(random_list)
 min_num, max_num = list(
     map(int, input('Enter the min and max elements of the list separated in the space: ').split(' ')))
-count_index_list = 0
 count_index_list = [i for i in range(len(random_list)) if (min_num <= random_list[i] <= max_num)]
 print(count_index_list)
